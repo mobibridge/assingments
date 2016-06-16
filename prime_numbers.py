819b1fb9c702c60eb2750a94e56ff53a4a2db6dd
Hillary <hillarymuriu@gmail.com>
2016-06-16 18:19:06 +0100

    Testing prime numbers

--- prime_numbers.py
+++ prime_numbers.py
@@ -2,13 +2,24 @@
 lowernumber = int(input("Enter lower range: "))
 uppernumber = int(input("Enter upper range: "))
 def prime(small,large):
+    prime_nos=[]
     for x in range(small,large + 1):
        # prime numbers are greater than 1
+       
+       isPrime=True
+
        if x > 1:
            for i in range(2,x):
                if (x % i) == 0:
-                   break
-           else:
-               print(x)
-      
-prime(lowernumber,uppernumber)
+                    isPrime=False
+                    break
+
+           if isPrime:
+                prime_nos.append(x)
+
+    return prime_nos
+
+                
+
+       
+print(prime(lowernumber,uppernumber))
